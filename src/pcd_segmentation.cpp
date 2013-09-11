edf9e8ff1f338b7f80820a32d2d51a6e4a4915cf
boddmg <boddmg@gmail.com>
2013-09-11 10:20:27 +0800

    1.封装完成 2.接下来用kinect实时测试 3.优化算法 4.写出球的半径、球心的获取函数 5.写出显示球的函数
    Signed-off-by: boddmg <boddmg@gmail.com>

--- src/pcd_segmentation.cpp
+++ src/pcd_segmentation.cpp
@@ -33,33 +33,33 @@ typedef pcl::PointXYZRGB PointT;
 //机械臂显示类
 class RobotViewer
 {
-	public:
+public:
 	EIGEN_MAKE_ALIGNED_OPERATOR_NEW 
-	boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer;//显示窗口的智能指针
+		boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer;//显示窗口的智能指针
 	RobotViewer()
 	{
-	// ------------------------------------
-	// -----Create blank point cloud-----
-	// ------------------------------------
-	pcl::PointCloud<pcl::PointXYZ>::Ptr basic_cloud_ptr (new pcl::PointCloud<pcl::PointXYZ>);
-	pcl::PointCloud<PointT>::Ptr point_cloud_ptr (new pcl::PointCloud<PointT>);
-	boost::shared_ptr<pcl::visualization::PCLVisualizer> viewerTemp(new pcl::visualization::PCLVisualizer ("Robot Viewer"));
+		// ------------------------------------
+		// -----Create blank point cloud-----
+		// ------------------------------------
+		pcl::PointCloud<pcl::PointXYZ>::Ptr basic_cloud_ptr (new pcl::PointCloud<pcl::PointXYZ>);
+		pcl::PointCloud<PointT>::Ptr point_cloud_ptr (new pcl::PointCloud<PointT>);
+		boost::shared_ptr<pcl::visualization::PCLVisualizer> viewerTemp(new pcl::visualization::PCLVisualizer ("Robot Viewer"));
 
-	viewerTemp->setBackgroundColor (0, 0, 0);
+		viewerTemp->setBackgroundColor (0, 0, 0);
 
-	pcl::visualization::PointCloudColorHandlerRGBField<PointT> rgb(point_cloud_ptr);						//创建一个空白的点云,并取得
+		pcl::visualization::PointCloudColorHandlerRGBField<PointT> rgb(point_cloud_ptr);						//创建一个空白的点云,并取得
 
-	viewerTemp->addPointCloud<PointT> (point_cloud_ptr, rgb, "kinect");
-	viewerTemp->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 3, "kinect");
+		viewerTemp->addPointCloud<PointT> (point_cloud_ptr, rgb, "kinect");
+		viewerTemp->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 3, "kinect");
 
 
-	Eigen::Affine3f transform;									//变换用仿射矩阵
-	std::vector< pcl::visualization::Camera > cameras;
+		Eigen::Affine3f transform;									//变换用仿射矩阵
+		std::vector< pcl::visualization::Camera > cameras;
 
-	viewerTemp->addCoordinateSystem(100.0);
-	viewerTemp->initCameraParameters();
-	viewerTemp->setCameraPosition(0,0,-2500,0,1,0);				//相机位姿变换
-	viewer=viewerTemp;
+		viewerTemp->addCoordinateSystem(100.0);
+		viewerTemp->initCameraParameters();
+		viewerTemp->setCameraPosition(0,0,-2500,0,1,0);				//相机位姿变换
+		viewer=viewerTemp;
 	}
 
 	void spinOnce()
@@ -84,21 +84,21 @@ protected:
 	{
 		Eigen::Matrix4f transformMatrix;
 		transformMatrix<<
-					1000,0,0,0,
-					0,1000,0,0,
-					0,0,1000,0,
-					0,0,0,1
-					;
+			1000,0,0,0,
+			0,1000,0,0,
+			0,0,1000,0,
+			0,0,0,1
+			;
 		transformMatrix=pcl::getTransformation(0,0,0,0,0,M_PI).matrix()*transformMatrix;
 		pcl::transformPointCloud(_cloud,_cloud,transformMatrix);
 	}
 public:
 	EIGEN_MAKE_ALIGNED_OPERATOR_NEW 
-	pointCloudGrabber()
+		pointCloudGrabber()
 	{
 		isUpdatedFlag=false;
 	}
-//	virtual void grab()=0;
+	//	virtual void grab()=0;
 
 	pcl::PointCloud<PointT> getPointCloud()
 	{
@@ -124,7 +124,7 @@ private:
 	string fileName;
 public:
 	EIGEN_MAKE_ALIGNED_OPERATOR_NEW 
-	FileGrabber(string _fileName):fileName(_fileName)
+		FileGrabber(string _fileName):fileName(_fileName)
 	{
 	}
 	FileGrabber()
@@ -156,44 +156,123 @@ string getNowTimeStr()
 	return timeStr;
 }
 
-#define CLAMP_COLOR_VALUE(v) (v) = (v)/255;
+void debug_printf(string text)
+{
+	static bool firstOpen=true;
+	ofstream oFile("log.txt",ios::app);
+	string timeStr=getNowTimeStr();
+	if (firstOpen)
+	{
+		char time[128],date[128];
+		_strtime(time); 
+		_strdate(date); 
+		oFile<<"-------------------"<<date<<" "<<time<<"------------------"<<endl;//debug
+		firstOpen=false;
+	}
+	oFile<<timeStr<<text<<endl;//debug
+	cout<<timeStr<<text<<endl;//debug
+}
+
+
 
 #define MAX(x,y) (x)>(y)?(x):(y)
 #define MIN(x,y) (x)<(y)?(x):(y)
 
-#define fequ(x,y,tol) ( ((x)*(1-(tol))<(y))&&((x)*(1+(tol))>(y)) )
+//#define fequ(x,y,tol) ( ((x)*(1-(tol))<(y))&&((x)*(1+(tol))>(y)) )
 
-void RGB2HSL(float r, float g, float b,
-             float *h, float *s, float *l) {
-    CLAMP_COLOR_VALUE(r);
-    CLAMP_COLOR_VALUE(g);
-    CLAMP_COLOR_VALUE(b);
-    
-    float max, min, delta, sum;
-    max = MAX(r, MAX(g, b));
-    min = MIN(r, MIN(g, b));
-    delta = max - min;
-    sum = max + min;
-    
-    *l = sum / 2;           // Lightness
-    if (delta == 0) {       // No Saturation, so Hue is undefined (achromatic)
-        *h = *s = 0;
-        return;
-    }
-    *s = delta / (sum < 1 ? sum : 2 - sum);             // Saturation
-    if      (r == max) *h = (g - b) / delta / 6;        // color between y & m
-    else if (g == max) *h = (2 + (b - r) / delta) / 6;  // color between c & y
-    else               *h = (4 + (r - g) / delta) / 6;  // color between m & y
-    if (*h < 0) *h += 1;
-}
-class BallRecongnition
+class BallRecongniter
 {
-	pcl::PointCloud<PointT> inCloud;
-	void HSLfilter()
+public:
+void CLAMP_COLOR_VALUE(float &v)
 	{
-
+		v=v/255.0;
 	}
+
+	inline bool fequ(float x,float y,float tol) 
+	{
+		return 	( ((x)*(1-(tol))<(y))&&((x)*(1+(tol))>(y)) );
+	}
+
+	inline void RGB2HSL(float r, float g, float b,
+		float *h, float *s, float *l) 
+	{
+		CLAMP_COLOR_VALUE(r);
+		CLAMP_COLOR_VALUE(g);
+		CLAMP_COLOR_VALUE(b);
+
+		float max, min, delta, sum;
+		max = MAX(r, MAX(g, b));
+		min = MIN(r, MIN(g, b));
+		delta = max - min;
+		sum = max + min;
+
+		*l = sum / 2;           // Lightness
+		if (delta == 0) 
+		{       // No Saturation, so Hue is undefined (achromatic)
+			*h = *s = 0;
+			return;
+		}
+		*s = delta / (sum < 1 ? sum : 2 - sum);             // Saturation
+		if      (r == max) *h = (g - b) / delta / 6;        // color between y & m
+		else if (g == max) *h = (2 + (b - r) / delta) / 6;  // color between c & y
+		else               *h = (4 + (r - g) / delta) / 6;  // color between m & y
+		if (*h < 0) *h += 1;
+	}
+
+	void FilterWithColorInHSL(pcl::PointCloud<PointT> &inCloud,pcl::PointCloud<PointT> &outCloud,float inH,float inTol)
+	{
+		//过滤球的颜色以外的对象
+		for (pcl::PointCloud<PointT>::iterator i=inCloud.begin();i!=inCloud.end();++i)
+		{
+			float h,s,l;
+			float tol=0.1;
+			RGB2HSL(i->r,i->g,i->b,&h,&s,&l);
+
+			if(fequ(h,inH,inTol) && pcl::isFinite(*i))
+			{
+				//i->r=0;
+				outCloud.insert(outCloud.end(),*i);
+			}
+		}	
+	}
+
+	void FilterRemovalPointsWithRadius(pcl::PointCloud<PointT> &inCloud,pcl::PointCloud<PointT> &outCloud,float radiusSearch,float minNeighborsInRadius)
+	{
+		pcl::RadiusOutlierRemoval<PointT> outrem(true);
+		// build the filter
+		outrem.setInputCloud(inCloud.makeShared());
+		outrem.setRadiusSearch(radiusSearch);
+		outrem.setMinNeighborsInRadius (minNeighborsInRadius);
+		outrem.setKeepOrganized(false);
+		// apply filter
+		outrem.filter (outCloud);
+	}
+
+	void SphereSegmentation(pcl::PointCloud<PointT> &inCloud,pcl::PointCloud<PointT> &outCloud,pcl::ModelCoefficients &coefficients_sphere)
+	{
+		pcl::SACSegmentation<PointT> seg; 
+		pcl::PointCloud<pcl::Normal>::Ptr cloud_normals;
+		pcl::ExtractIndices<PointT> extract;
+		pcl::PointIndices::Ptr inliers_sphere (new pcl::PointIndices);
+		pcl::PointCloud<PointT>::Ptr cloud_sphere (new pcl::PointCloud<PointT> ());
+
+		seg.setOptimizeCoefficients (true);
+		seg.setModelType (pcl::SACMODEL_SPHERE);
+		seg.setMethodType (pcl::SAC_RANSAC);
+		seg.setMaxIterations (100);
+		seg.setDistanceThreshold (5);
+		seg.setRadiusLimits (20, 40);
+		seg.setInputCloud (inCloud.makeShared());
+		seg.segment (*inliers_sphere, coefficients_sphere);
+		
+		extract.setInputCloud (inCloud.makeShared());
+		extract.setIndices (inliers_sphere);
+		extract.setNegative (false);
+		extract.filter (outCloud);
+	}
+
 };
+
 int main (int argc, char** argv)
 {
 	uint32_t i=0;
@@ -203,96 +282,59 @@ int main (int argc, char** argv)
 	FileGrabber fileGrabber;
 	RobotViewer viewer1;
 	RobotViewer viewer2;
-
-	cout<<argc<<endl;
+	stringstream stringBuilder;
+	BallRecongniter ballRecongniter;
 	if(argc>=2)
 	{
 		fileGrabber.setFileName(argv[1]);
 		fileGrabber.grab();
-		 
-		cout<<getNowTimeStr()<<"Start"<<endl;
-		cout<<getNowTimeStr()<<"color filter"<<endl;//debug
+		debug_printf(argv[1]);
+		debug_printf("Start");
+		debug_printf("color filter");//debug
+
 		pcl::PointCloud<PointT> _cloud(fileGrabber.getPointCloud());
 		pcl::PointCloud<PointT> filteredCloud;
-		filteredCloud.empty();
-		//过滤球的颜色以外的对象
-		for (pcl::PointCloud<PointT>::iterator i=_cloud.begin();i!=_cloud.end();++i)
-		{
-			float h,s,l;
-			float tol=0.1;
-			RGB2HSL(i->r,i->g,i->b,&h,&s,&l);
-			
-			if(fequ(h,60.0/360,0.1) && pcl::isFinite(*i))
-			{
-				//i->r=0;
-				filteredCloud.insert(filteredCloud.end(),*i);
-			}
-		}
-		cout<<"points size before filter"<<_cloud.points.size()<<endl;//debug
 
-		cout<<"now filterd points size"<<filteredCloud.points.size()<<endl;//debug
-		
-		cout<<getNowTimeStr()<<"RadiusOutlierRemoval"<<endl;//debug
+		ballRecongniter.FilterWithColorInHSL(_cloud,filteredCloud,0.1667,0.1);
+		stringBuilder<<"points size before filtering:"<<_cloud.points.size()<<endl;//debug
+		debug_printf(stringBuilder.str());
+		stringBuilder.str("");
+		stringBuilder.clear();
+
+		stringBuilder<<"points size after filtering:"<<filteredCloud.points.size()<<endl;//debug
+		debug_printf(stringBuilder.str());
+		stringBuilder.str("");
+		stringBuilder.clear();
+
+		debug_printf("RadiusOutlierRemoval");//debug
+
 		//过滤离群的点
-		pcl::RadiusOutlierRemoval<PointT> outrem(true);
-		// build the filter
-		outrem.setInputCloud(filteredCloud.makeShared());
-		outrem.setRadiusSearch(5);
-		outrem.setMinNeighborsInRadius (5);
-		outrem.setKeepOrganized(false);
-		// apply filter
-		outrem.filter (filteredCloud);
+		ballRecongniter.FilterRemovalPointsWithRadius(filteredCloud,filteredCloud,5,5);
+		debug_printf("over");//debug		
+		stringBuilder<<"points size after filtering:"<<filteredCloud.points.size()<<endl;//debug
+		debug_printf(stringBuilder.str());
+		stringBuilder.str("");
+		stringBuilder.clear();
 
-		cout<<getNowTimeStr()<<"over"<<endl;//debug
-		
-		
 		//Start to segmentation
-		pcl::SACSegmentation<PointT> seg; 
-		pcl::PointCloud<pcl::Normal>::Ptr cloud_normals;
-		pcl::ExtractIndices<PointT> extract;
-		pcl::PointIndices::Ptr inliers_sphere (new pcl::PointIndices);
-		pcl::ModelCoefficients::Ptr coefficients_sphere (new pcl::ModelCoefficients);
-		pcl::PointCloud<PointT>::Ptr cloud_sphere (new pcl::PointCloud<PointT> ());
-
-		cout<<getNowTimeStr()<<"Set segment params"<<endl;//debug
-		seg.setOptimizeCoefficients (true);
-		seg.setModelType (pcl::SACMODEL_SPHERE);
-		seg.setMethodType (pcl::SAC_RANSAC);
-		seg.setMaxIterations (100);
-		seg.setDistanceThreshold (5);
-		seg.setRadiusLimits (20, 40);
-		seg.setInputCloud (filteredCloud.makeShared());
-		cout<<getNowTimeStr()<<"Set segment params done...."<<endl;//debug
-
-		cout<<getNowTimeStr()<<"Start segment"<<endl;//debug
-		seg.segment (*inliers_sphere, *coefficients_sphere);
-		cout<<getNowTimeStr()<<"Segment done...."<<endl;//debug
-		
-
-		cout<<getNowTimeStr()<<"Start extract"<<endl;//debug
-
-		extract.setInputCloud (filteredCloud.makeShared());
-		extract.setIndices (inliers_sphere);
-		extract.setNegative (false);
-		extract.filter (filteredCloud);
-
-
-		cout<<"x:"<<coefficients_sphere->values[0]<<endl;//debug
-		cout<<"y:"<<coefficients_sphere->values[1]<<endl;//debug
-		cout<<"z:"<<coefficients_sphere->values[2]<<endl;//debug
-		cout<<"radius:"<<coefficients_sphere->values[3]<<endl;//debug
+		pcl::ModelCoefficients coefficients_sphere;
+		ballRecongniter.SphereSegmentation(filteredCloud,filteredCloud,coefficients_sphere);
+		stringBuilder<<"radius:"<<coefficients_sphere.values[3]<<endl;//debug
+		debug_printf(stringBuilder.str());
+		stringBuilder.str("");
+		stringBuilder.clear();
 
 		//画出球心
-		PointT *sphereCenter=new PointT();
-		sphereCenter->x=coefficients_sphere->values[0];
-		sphereCenter->y=coefficients_sphere->values[1];
-		sphereCenter->z=coefficients_sphere->values[2];
-		sphereCenter->rgb=0xffffff; 
+		PointT sphereCenter;
+		sphereCenter.x=coefficients_sphere.values[0];
+		sphereCenter.y=coefficients_sphere.values[1];
+		sphereCenter.z=coefficients_sphere.values[2];
+		sphereCenter.rgb=0xffffff; 
 
-		filteredCloud.insert(filteredCloud.end() ,*sphereCenter);
+		filteredCloud.insert(filteredCloud.end() ,sphereCenter);
 
-		cout<<getNowTimeStr()<<"Extract done....."<<endl;//debug
-		
+		debug_printf("Extract done.....");//debug
+
 
 		viewer1.updateBGcloud(_cloud);
 		viewer2.updateBGcloud(filteredCloud);
